e5dc9e8ff3d98a2be85c92e84c0b8470389f1aec
Angelo Rafael <angelorpt@gmail.com>
2018-05-03 20:17:52 -0400

    Código para ocultar ou desabilitar itens do menu do Delphi 7

---
+++ SourceCode/OcultarMenu.pas
@@ -0,0 +1,68 @@
+{
+  Angelo R. Pinto
+  Percorre o menu e seus submenus procurando o nome do ItemMenu passado como parâmetro.
+  Quando localiza altera o valor da propriedade Visible ou Enabled
+}
+
+procedure subItemMenu_VE(menu: TMenuItem; nomeItem: String; propriedade: integer; valor_Propriedade: Boolean);
+var
+  i: integer;
+begin
+{ propriedade:  1 - Visible / 2 - Enabled  }
+  for i:= 0 to menu.Count-1 do begin
+    subItemMenu_VE(menu[i], nomeItem, propriedade, valor_Propriedade);
+    if UpperCase(menu[i].Name) = UpperCase(nomeItem) then begin
+      case propriedade of
+        1: menu.Items[i].Visible := valor_Propriedade;
+        2: menu.Items[i].Enabled := valor_Propriedade;
+      end;
+      break;
+    end;
+  end;
+end;
+
+procedure itemMenu_VE(menu: TMenu; nomeItem: String; propriedade: integer; valor_Propriedade: Boolean);
+var
+  i: integer;
+begin
+
+{ propriedade:  1 - Visible / 2 - Enabled  }
+
+  for i:= 0 to menu.Items.Count-1 do begin
+    subItemMenu_VE(menu.Items[i], nomeItem, propriedade, valor_Propriedade);
+    if UpperCase(menu.Items[i].Name) = UpperCase(nomeItem) then begin
+      case propriedade of
+        1: menu.Items[i].Visible := valor_Propriedade;
+        2: menu.Items[i].Enabled := valor_Propriedade;
+      end;
+      break;
+    end;
+  end;
+end;
+
+// --------------
+// BUTTONS
+// --------------
+
+procedure TfrmMenu.btnOKClick(Sender: TObject);
+begin
+
+  // MainMenu1 - Nomo do componente Menu
+
+  if opt_Visible.Checked = True then begin
+    if opt_True.Checked = True then begin
+      itemMenu_VE(MainMenu1, txt_Botao.Text, 1, True);
+    end else begin
+      itemMenu_VE(MainMenu1, txt_Botao.Text, 1, False);
+    end;
+  end;
+
+  if opt_Enabled.Checked = True then begin
+    if opt_True.Checked = True then begin
+      itemMenu_VE(MainMenu1, txt_Botao.Text, 2, True);
+    end else begin
+      itemMenu_VE(MainMenu1, txt_Botao.Text, 2, False);
+    end;
+  end;
+
+end;
